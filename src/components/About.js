b2812e7abb7fc3315c7b1515d090fc7b361e86d9
Chandra Lama <iamme24cl@gmail.com>
2021-07-09 21:07:48 -0700

    changed font style and adjusted the project images

--- src/components/About.js
+++ src/components/About.js
@@ -39,11 +39,11 @@ const About = () => {
         <h3 className="w3-padding-24 w3-text-light-grey">My Reputation</h3>  
 
         <img src="https://randomuser.me/api/portraits/men/85.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width:"80px"}}/>
-        <p><span className="w3-large w3-margin-right">Chris Fox.</span> CEO at Mighty Schools.</p>
+        <p><span className="w3-large w3-margin-right">Chris Fox.</span><small>CEO at Mighty Schools.</small></p>
         <p>Chandra saved us from a web disaster.</p><br/>
         
         <img src="https://randomuser.me/api/portraits/women/19.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width:"80px"}}/>
-        <p><span className="w3-large w3-margin-right">Rebecca Flex.</span> CEO at Company.</p>
+        <p><span className="w3-large w3-margin-right"><small>Rebecca Flex.</small></span> CEO at Company.</p>
         <p>No one is better than Chandra Lama.</p>
       {/* <!-- End About Section --> */}
     </div>  
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,19 +3,35 @@ import React from 'react';
 const Projects = () => {
   return (
     <div className="w3-padding-64 w3-content" id="projects">
-      <h2 className="w3-text-light-grey">My Projects</h2>
-      <hr style={{width: "200px"}} className="w3-opacity"/>
+      <h2 className="w3-text-light-grey w3-center">My Projects</h2>
+      <hr style={{width: "100%"}} className="w3-opacity"/>
 
       {/* Grid for projects */}
       <div className="w3-row-padding" style={{margin: "0 -16px"}}>
-        <div className="w3-half">
+        <div className="w3-half w3-card-4">
           <img src="https://picsum.photos/id/0/150/150" style={{width: "100%"}} />
+          <div className="w3-container">
+            <p className="w3-center">This is my project.</p>
+          </div>
+          <hr style={{width: "100%"}} className="w3-opacity"/>
           <img src="https://picsum.photos/id/1033/150/150" style={{width: "100%"}} />
+          <div className="w3-container">
+            <p className="w3-center">This is my project.</p>
+          </div>
+          <hr style={{width: "100%"}} className="w3-opacity"/>
         </div>
 
-        <div className="w3-half">
+        <div className="w3-half w3-card-4">
           <img src="https://picsum.photos/id/158/150/150" style={{width: "100%"}} />
+          <div className="w3-container">
+            <p className="w3-center">This is my project.</p>
+          </div>
+          <hr style={{width: "100%"}} className="w3-opacity"/>
           <img src="https://picsum.photos/id/1048/150/150" style={{width: "100%"}} />
+          <div className="w3-container">
+            <p className="w3-center">This is my project.</p>
+          </div>
+          <hr style={{width: "100%"}} className="w3-opacity"/>
         </div>
       {/* End photo grid  */}
       </div>
