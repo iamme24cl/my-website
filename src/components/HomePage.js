0ed48b0f547b024e370924e1bbba8595a50d8fd8
Chandra Lama <iamme24cl@gmail.com>
2021-08-24 12:53:55 -0700

    changed home page image and added css animation to make the image float

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
       <header className="w3-container w3-padding-32 w3-center w3-black" id="home">
         <h1 className="w3-jumbo"><span>Hi!<span className="wave"> 👋</span> I'm</span> Chandra Lama.<span>&#128512;</span></h1>
         <p>FullStack Software Developer.</p>
-        <img src="Photo_1619833647159_Processed.png" alt="boy" className="w3-image my-image" width="450" height="650" />
+        <img src="software-engineers-vector.png" alt="boy" className="w3-image my-image" width="450" height="650" />
       </header>
     </div>
   );
