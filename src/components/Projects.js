6fb8b7c2064422f0d6712943380b1a190cf0b6d0
Chandra Lama <iamme24cl@gmail.com>
2021-09-17 02:19:15 -0700

    .

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,14 @@ const Projects = () => {
       {/* Grid for projects */}
       <div className="w3-row-padding" style={{margin: "0 -16px"}}>
         <div className="w3-half w3-card-4">
+          <a href="https://my-invoices.netlify.app/" target="_blank" rel="noreferrer"><img className="project-image" src="images/invoices.png" style={{width: "100%"}} alt="invoices" /></a>
+          <div className="w3-container">
+            <p className="w3-center">A single page application with React and Redux front end that creates and manages Invoices for an Account. 
+              User can also filter their invoices by Client name and Invoice description.</p>
+          </div>
+        </div>
+
+        <div className="w3-half w3-card-4">
           <a href="https://iamme24cl.github.io/expense-tracker-frontend/" target="_blank" rel="noreferrer"><img className="project-image" src="images/expense-tracker.png" style={{width: "100%"}} alt="expense-tracker" /></a>
           <div className="w3-container">
             <p className="w3-center">Keep track of your income and expenses! This is a small 
@@ -17,24 +25,15 @@ const Projects = () => {
             from api built with Ruby on Rails.</p>
           </div>
         </div>
-
-        <div className="w3-half w3-card-4">
-          <a href="https://www.youtube.com/watch?v=i3Q8Ns4z11o" target="_blank" rel="noreferrer"><img className="project-image" src="images/recipes.png" style={{width: "100%"}} alt="recipe-manager" /></a>
-          <div className="w3-container">
-            <p className="w3-center">Recipe manager app with user account functionality that lets you rate and review recipes and filter recipes.
-              User has many recipes and can create new or add an existing recipe and manage their recipes list.</p>
-          </div>
-        </div>
-      </div>
-
       <hr style={{width: "100%"}} className="w3-opacity"/>
 
       <div className="w3-row-padding" style={{margin: "0 -16px"}}>
         <div className="w3-half w3-card-4">
-          <a href="https://my-invoices.netlify.app/" target="_blank" rel="noreferrer"><img className="project-image" src="images/invoices.png" style={{width: "100%"}} alt="invoices" /></a>
-          <div className="w3-container">
-            <p className="w3-center">A single page application with React and Redux front end that creates and manages Invoices for an Account. 
-              User can also filter their invoices by Client name and Invoice description.</p>
+            <a href="https://www.youtube.com/watch?v=i3Q8Ns4z11o" target="_blank" rel="noreferrer"><img className="project-image" src="images/recipes.png" style={{width: "100%"}} alt="recipe-manager" /></a>
+            <div className="w3-container">
+              <p className="w3-center">Recipe manager app with user account functionality that lets you rate and review recipes and filter recipes.
+                User has many recipes and can create new or add an existing recipe and manage their recipes list.</p>
+            </div>
           </div>
         </div>
 
