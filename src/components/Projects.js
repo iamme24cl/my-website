70414c733b0699248c4196252e2f98583cdc0724
Chandra Lama <iamme24cl@gmail.com>
2021-10-04 10:03:20 -0700

    Changed project image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
         <div className="w3-half w3-card-4">
         <a href="https://my-invoices.netlify.app/" target="_blank" rel="noreferrer"><img className="project-image" src="images/invoices.gif" style={{width: "100%", height: "300px"}} alt="invoices" /></a>
           <div className="w3-container">
-            <p className="w3-center">A single page application with React and Redux front end that creates and manages Invoices for an Account. 
+            <p className="w3-center">A single page application with React and Redux front end that creates and manages Invoices for an Account.
               User can also filter their invoices by Client name and Invoice description.</p>
           </div>
         </div>
@@ -20,18 +20,18 @@ const Projects = () => {
         <div className="w3-half w3-card-4">
           <a href="https://iamme24cl.github.io/expense-tracker-frontend/" target="_blank" rel="noreferrer"><img className="project-image" src="images/expense-tracker.gif" style={{width: "100%", height: "300px"}} alt="expense-tracker" /></a>
           <div className="w3-container">
-            <p className="w3-center">Keep track of your income and expenses! This is a small 
-            one page application. The front end is built with vanilla JavaScript and fetches data 
+            <p className="w3-center">Keep track of your income and expenses! This is a small
+            one page application. The front end is built with vanilla JavaScript and fetches data
             from api built with Ruby on Rails.</p>
           </div>
         </div>
       </div>
-        
+
       <hr style={{width: "100%"}} className="w3-opacity"/>
 
       <div className="w3-row-padding" style={{margin: "0 -16px"}}>
         <div className="w3-half w3-card-4">
-          <a href="https://www.youtube.com/watch?v=i3Q8Ns4z11o" target="_blank" rel="noreferrer"><img className="project-image" src="images/recipes.png" style={{width: "100%", height: "300px"}} alt="recipe-manager" /></a>
+          <a href="https://my-recipes-2020.herokuapp.com/" target="_blank" rel="noreferrer"><img className="project-image" src="images/recipes.gif" style={{width: "100%", height: "300px"}} alt="recipe-manager" /></a>
           <div className="w3-container">
             <p className="w3-center">Recipe manager app with user account functionality that lets you rate and review recipes and filter recipes.
             User has many recipes and can create new or add an existing recipe and manage their recipes list.</p>
@@ -48,7 +48,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      
+
       <hr style={{width: "100%"}} className="w3-opacity"/>
     </div>
   )
