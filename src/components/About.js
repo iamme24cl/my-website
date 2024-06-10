d4b59e4d847984f97e89fc035fc802a61493ca31
Chandra Lama <iamme24cl@gmail.com>
2024-06-10 13:09:43 -0700

    changed about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,15 @@ import "./About.css";
 
 const About = () => {
   return (
-     <div className="w3-content w3-text-light-grey" id="about">
+     <div className="about-container w3-content w3-text-light-grey" id="about">
       <h2 className='w3-center'>About Me</h2>
         <hr style={{ width:"100%" }} className="w3-opacity"/>
         <section className="w3-text-light-grey">
           <p>
-          Detail-oriented and highly motivated full stack software developer. Proficient in Python for rapid implementation and iteration, 
-          experienced in React for building interactive front-ends, and skilled in web programming. 
+            Detail-oriented and highly motivated full stack software developer. Proficient in Python for rapid implementation and iteration, 
+            experienced in React for building interactive front-ends, and skilled in web programming. 
+            Adept at backend system design and implementation with a passion for continuous learning and skill expansion.
           </p>
-          <p>Adept at backend system design and implementation with a passion for continuous learning and skill expansion.</p>
         </section>
     </div>
   );
