f2fe89244e8d031203def7ec8214f25a8788106c
Chandra Lama <iamme24cl@gmail.com>
2024-08-30 14:42:52 -0700

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,11 @@ const About = () => {
         <hr style={{ width:"100%" }} className="w3-opacity"/>
         <section className="w3-text-light-grey">
           <p>
-            With over 15 years of professional experience, transitioning from hospitality to software engineering has been a thrilling journey. 
-            The customer-facing skills I've developed over the years uniquely position me to understand user needs and deliver intuitive, 
-            user-friendly software solutions. Now, I’m looking to bring this combination of technical and interpersonal skills to a forward-thinking company. 
-            If you're searching for a software engineer who brings a unique perspective to the table, let's connect!
+          With over 15 years of professional experience, including the last 3 years as a software developer, transitioning from hospitality to software engineering 
+          has been a thrilling journey. The customer-facing skills I've honed over the years uniquely position me to understand user needs and deliver intuitive, 
+          user-friendly software solutions. My recent experience in software development has solidified my technical expertise, allowing me to work with Python, 
+          React, Node.js, and various other technologies to build robust applications. Now, I’m looking to bring this combination of technical and interpersonal skills 
+          to a forward-thinking company. If you're searching for a software engineer who brings a unique perspective to the table, let's connect!
           </p>
         </section>
     </div>
